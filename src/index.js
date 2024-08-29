219f63577b016d335884f6ab4c849fa213ddcdcb
joyjitdas3918 <138395405+joyjitdas3918@users.noreply.github.com>
2024-08-29 11:34:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ root.render(
   </React.StrictMode>
 );
 
+const url = `https://phodit.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
